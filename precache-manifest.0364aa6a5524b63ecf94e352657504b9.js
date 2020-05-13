3327c924bf0ed859ac83441674d1ceb866004511
kruanurak <akhophadung@gmail.com>
2020-05-13 21:13:25 +0700

    Updates

---
+++ precache-manifest.0364aa6a5524b63ecf94e352657504b9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "263139de282bb9ba692db9e0b09223bb",
+    "url": "/pkstudent-email/index.html"
+  },
+  {
+    "revision": "8e6ff4c5433cbac1829c",
+    "url": "/pkstudent-email/static/css/2.ecf09bb4.chunk.css"
+  },
+  {
+    "revision": "f1d8ab2683ddd13256b5",
+    "url": "/pkstudent-email/static/css/main.d1a190db.chunk.css"
+  },
+  {
+    "revision": "8e6ff4c5433cbac1829c",
+    "url": "/pkstudent-email/static/js/2.d9351770.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/pkstudent-email/static/js/2.d9351770.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1d8ab2683ddd13256b5",
+    "url": "/pkstudent-email/static/js/main.d5bcea09.chunk.js"
+  },
+  {
+    "revision": "e9f0b507a3e15c43010a",
+    "url": "/pkstudent-email/static/js/runtime-main.955d2d32.js"
+  },
+  {
+    "revision": "49d082d0164cdf471f5e00b1fddb89f9",
+    "url": "/pkstudent-email/static/media/Sarabun-Regular.49d082d0.ttf"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pkstudent-email/static/media/logo.5d5d9eef.svg"
+  }
+]);
